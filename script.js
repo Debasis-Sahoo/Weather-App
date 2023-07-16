45f31da8a83af258732dcc8b2836bbd051c6c09a
Debasis Sahoo <100115917+Debasis-Sahoo@users.noreply.github.com>
2023-07-16 16:36:29 +0000

    add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apikey = "863242cfb2b1d357e6093d9a4df19a4b"; 
+const apikey = "614a85ccbf2442b04dae25e970b164a3"; 
 
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?nits=metric&q=bangalore";
 
